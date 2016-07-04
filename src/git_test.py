01fed76fd7d42dcf2a0377cd0c63e2be9aac3dc5
michael lee <keymikey54@yahoo.com>
2016-07-04 11:14:28 -0400

    just a simple commit

--- src/git_test.py
+++ src/git_test.py
@@ -4,10 +4,11 @@ Created on Jul 4, 2016
 @author: leem42
 '''
 
-def main():
-    print 'hello'
-    print 'this is to test out the first usage of github'
-    print 'update this to main branch'
+import os, one
 
-if __name__ == '__main__':
-    pass
+def main():
+    print 'git test ran'
+
+if __name__ == "__main__":
+    main()
+    one.main('hello' 'two')
---
+++ src/one.py
@@ -0,0 +1,12 @@
+'''
+Created on Jul 4, 2016
+
+@author: leem42
+'''
+
+import sys
+def main(argv):
+    print argv
+    for arg in sys.argv[1:]:
+            print arg
+    print 'hi'
