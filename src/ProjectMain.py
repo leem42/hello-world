f09b073a0cdb46ba40690cdc562b55ac3eca1d95
michael lee <keymikey54@yahoo.com>
2016-07-24 15:02:16 -0400

    hello added

--- src/ProjectMain.py
+++ src/ProjectMain.py
@@ -4,9 +4,10 @@ Created on Jul 24, 2016
 @author: leem42
 '''
 
-def main():
-    print 'I just ran :D'
+def main(argv):
+    
+    print 'I just ran ' + str(argv)
 
 
 if __name__ == '__main__':
-    main()
+    main('HELLO')
