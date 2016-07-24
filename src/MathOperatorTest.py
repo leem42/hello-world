23359d59e923fd7a43259001b5ae4805d8c5ad69
michael lee <keymikey54@yahoo.com>
2016-07-24 14:59:57 -0400

    main added

--- src/MathOperatorTest.py
+++ src/MathOperatorTest.py
@@ -6,12 +6,14 @@ Created on Jul 17, 2016
 
 import unittest
 
-#===============================================================================
-# Class MathOperatorTest tests the functionality of class MathOperatorTest,
-# Performing both simple test cases and more complex opertaions
-#===============================================================================
+
 class MathOperatorTest(unittest.TestCase):
 
+    '''
+    Class MathOperatorTest tests the functionality of class MathOperatorTest,
+    Performing both simple test cases and more complex operations
+    
+    '''
 
     def testName(self):
         pass
---
+++ src/ProjectMain.py
@@ -0,0 +1,12 @@
+'''
+Created on Jul 24, 2016
+
+@author: leem42
+'''
+
+def main():
+    print 'I just ran :D'
+
+
+if __name__ == '__main__':
+    main()
